18e4e674aa3196855738edf83b5f238b74db8a42
Caelan <caelan.borowiec@pqi.org>
2016-01-28 11:55:24 -0500

    displaysuite & ddblock templates
    Added displaysuite layout template for homepage
    Added ddblock styles

---
+++ multipurpose/ds_layouts/homepage_leftsidebar/homepage-leftsidebar.tpl.php
@@ -0,0 +1,49 @@
+
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-profile <?php print $classes;?> clearfix">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+  <?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+  
+
+  
+  <div>
+	<div id="main_header">
+	<<?php print $mainheaderspan_wrapper ?> class="group-mainheaderspan<?php print $mainheaderspan_classes; ?>">
+		<?php print $mainheaderspan; ?>
+	</<?php print $mainheaderspan_wrapper ?>>
+	</div>
+	<div id="left_sidebar">  
+		<<?php print $sidebar_wrapper ?> class="group-sidebar<?php print $sidebar_classes; ?>">
+			<?php print $sidebar; ?>
+		</<?php print $sidebar_wrapper ?>>
+	</div>
+  
+	<div id="right_main">
+		<div>
+			<!--<div class="section-title">Featured Stories</div>-->
+		  <<?php print $rightmain_wrapper ?> class="group-rightmain<?php print $rightmain_classes; ?>">
+			<?php print $rightmain; ?>
+		  </<?php print $rightmain_wrapper ?>>
+		</div>
+		
+		<div id="center_pane">
+		  <<?php print $centerbottom_wrapper ?> class="group-centerbottom<?php print $centerbottom_classes; ?>">
+			<?php print $centerbottom; ?>
+		  </<?php print $centerbottom_wrapper ?>>
+		</div>
+		<div id="right_pane">
+			<<?php print $centerright_wrapper ?> class="group-centerright<?php print $centerright_classes; ?>">
+			<?php print $centerright; ?>
+			</<?php print $centerright_wrapper ?>>
+		</div>
+	</div>
+
+
+  
+
+</<?php print $layout_wrapper ?>>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
