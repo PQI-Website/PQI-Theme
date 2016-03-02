f5ba3542dea49fd66a73fdeb038461fb60c9196e
Caelan <caelan.borowiec@pqi.org>
2016-03-02 15:18:05 -0500

    CSS, Timeago
    Added support for Fuzzy Dates using time ago
    Added styles for popular news
    Added more styles for storify

---
+++ multipurpose/js/custom.js
@@ -0,0 +1,34 @@
+// Implement Fuzzy Dates for storify
+jQuery(document).ready(function() {
+	jQuery('p#meta span').each(function() {
+		var postDate = new Date(jQuery(this).text());
+		jQuery(this).attr("datetime", postDate.toISOString()); 
+		jQuery(this).text(jQuery.timeago(postDate.toISOString())); 
+	});
+});
+/*
+jQuery(document).ready(function() {
+	jQuery('ul.meta li span[content]').each(function() {
+		var postDate = new Date(jQuery(this).attr("content"));
+		jQuery(this).attr("datetime", postDate.toISOString()); 
+		jQuery(this).text(jQuery.timeago(postDate.toISOString())); 
+	});
+});
+*/
+jQuery(window).bind("load", function() {
+	jQuery('div.block-ddblock div.slider-inner').each(function() {
+		var largestHeight = 0;
+		jQuery(this).find("div.slide").each(function(){
+			var slideHeight = 0;
+			jQuery(this).children().each(function(){
+				slideHeight += jQuery(this).outerHeight(true);
+			});
+			//console.log(slideHeight);
+			if (slideHeight > largestHeight)
+			{
+				largestHeight = slideHeight;
+			}
+		});
+		jQuery(this).animate({height:largestHeight},1000);//.height(largestHeight+"px");
+	});
+});
