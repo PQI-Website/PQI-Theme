08ac00b9d3d3fc4cd5dc974d0c67c6ef8c62bf17
Caelan <caelan.borowiec@pqi.org>
2016-04-06 13:10:32 -0400

    Added Right sidebar footer template

---
+++ multipurpose/ds_layouts/right_sidebar_footer/right-sidebar-footer.tpl.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * Display Suite 2 column template.
+ */
+
+  // Add sidebar classes so that we can apply the correct width in css.
+  if (($left && !$right) || ($right && !$left)) {
+    $classes .= ' group-one-column';
+  }
+?>
+<<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-2col-fluid <?php print $classes;?> clearfix">
+
+  <?php if (isset($title_suffix['contextual_links'])): ?>
+	<?php print render($title_suffix['contextual_links']); ?>
+  <?php endif; ?>
+
+<<?php print $left_wrapper ?> class="group-left<?php print $left_classes; ?>">
+  <?php print $left; ?>
+</<?php print $left_wrapper ?>>
+
+<?php if ($right): ?>
+<<?php print $right_wrapper ?> class="group-right<?php print $right_classes; ?>">
+  <?php print $right; ?>
+</<?php print $right_wrapper ?>>
+<?php endif; ?>
+
+</<?php print $layout_wrapper ?>>
+
+<?php if ($bottom): ?>
+<<?php print $bottom_wrapper ?> class="group-bottom<?php print $bottom_classes; ?>">
+  <?php print $bottom; ?>
+</<?php print $bottom_wrapper ?>>
+<?php endif; ?>
+
+<?php if (!empty($drupal_render_children)): ?>
+  <?php print $drupal_render_children ?>
+<?php endif; ?>
