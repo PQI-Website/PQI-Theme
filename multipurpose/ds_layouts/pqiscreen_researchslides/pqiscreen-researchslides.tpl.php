5cdaaded92978b0a6697d7d37cb4f076eefbbcfa
Caelan <caelan.borowiec@pqi.org>
2017-12-14 16:26:22 -0500

    Basic slide changing

--- multipurpose/ds_layouts/pqiscreen_researchslides/pqiscreen-researchslides.tpl.php
+++ multipurpose/ds_layouts/pqiscreen_researchslides/pqiscreen-researchslides.tpl.php
@@ -1,3 +1,15 @@
+<script>
+  $(".field-name-field-research-slideshow .field-items > div.field-item:gt(0)").hide();
+  function LoadSlide() {
+    $('.field-name-field-research-slideshow .field-items > div.field-item:first')
+      .fadeOut(1000)
+      .next()
+      .fadeIn(1000)
+      .end();
+      //.appendTo('.field-name-field-research-slideshow .field-items');
+  }
+  setInterval(LoadSlide(), 3000);
+</script>
 <<?php print $layout_wrapper; print $layout_attributes; ?> class="<?php print $classes;?> clearfix">
 
   <?php if (isset($title_suffix['contextual_links'])): ?>
