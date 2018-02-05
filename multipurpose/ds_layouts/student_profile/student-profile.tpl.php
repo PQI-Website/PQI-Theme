f3b2e34b7b7d69c26730f892d60726a0f845f71e
Caelan <caelan.borowiec@pqi.org>
2018-02-05 15:17:15 -0500

    Streamlined sections

--- multipurpose/ds_layouts/student_profile/student-profile.tpl.php
+++ multipurpose/ds_layouts/student_profile/student-profile.tpl.php
@@ -1,10 +1,3 @@
-<?php
-
-/**
- * @file
- * Display Suite 2 column template.
- */
-?>
 <<?php print $layout_wrapper; print $layout_attributes; ?> class="ds-profile <?php print $classes;?> clearfix">
 
   <?php if (isset($title_suffix['contextual_links'])): ?>
@@ -12,49 +5,35 @@
   <?php endif; ?>
 
 
-  <div id="contact-wrap">
   <<?php print $photo_wrapper ?> class="group-photo<?php print $photo_classes; ?>">
     <?php print $photo; ?>
   </<?php print $photo_wrapper ?>>
 
+  <div id="contact-wrap">
   <<?php print $contact_wrapper ?> class="group-contact<?php print $contact_classes; ?>">
     <?php print $contact; ?>
-	
-	<<?php print $websites_wrapper ?> class="group-websites<?php print $websites_classes; ?>">
-		<div class="field-label">Websites:&nbsp;</div>
-		<?php print $websites; ?>
-	</<?php print $websites_wrapper ?>>
   </<?php print $contact_wrapper ?>>
   </div>
 
   <<?php print $affiliation_wrapper ?> class="group-affiliation<?php print $affiliation_classes; ?>">
-  <div class="block-title">Affiliation:</div>
+  <div class="section-title">Affiliation:</div>
     <?php print $affiliation; ?>
   </<?php print $affiliation_wrapper ?>>
 
   <<?php print $education_wrapper ?> class="group-education<?php print $education_classes; ?>">
-  <div class="block-title">Education:</div>
+  <div class="section-title">Education:</div>
     <?php print $education; ?>
   </<?php print $education_wrapper ?>>
 
   <<?php print $research_wrapper ?> class="group-research<?php print $research_classes; ?>">
-  <div class="block-title">Research:</div>
+  <div class="section-title">Research:</div>
     <?php print $research; ?>
   </<?php print $research_wrapper ?>>
 
-  <div class="group-publications">
-	  <?php if ($publications_left): ?>
-	  <<?php print $publications_left_wrapper ?> class="group-publications_left<?php print $publications_left_classes; ?>">
-		<?php print $publications_left; ?>
-	  </<?php print $publications_left_wrapper ?>>
-	  <?php endif; ?>
-	  
-	  <?php if ($publications_right): ?>
-		  <<?php print $publications_right_wrapper ?> class="group-publications_right<?php print $publications_right_classes; ?>">
-			<?php print $publications_right; ?>
-		  </<?php print $publications_right_wrapper ?>>
-	  <?php endif; ?>
-  </div>
+  <<?php print $websites_wrapper ?> class="group-websites<?php print $websites_classes; ?>">
+		<div class="section-title">Websites:&nbsp;</div>
+		<?php print $websites; ?>
+	</<?php print $websites_wrapper ?>>
 
 </<?php print $layout_wrapper ?>>
 
